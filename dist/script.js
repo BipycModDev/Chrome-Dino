b72804f0a4a69a3108f4f1c527801f769be04980
BipycModDev <ukrainianguy20@gmail.com>
2025-05-13 11:59:45 +0300

    added death

--- dist/script.js
+++ dist/script.js
@@ -2,14 +2,43 @@ const dino = document.querySelector(".dino");
 const kaktyc = document.querySelector(".kaktyc");
 let isDzhamping = false;
 
-function dzhamp(){
-	if (isDzhamping) return;
-	dino.classList.add("dzhamp");
-	isDzhamping=true;
-	setTimeout(()=>{
-		dino.classList.remove("dzhamp");
-		isDzhamping=false;
-	}, 500);
+function dzhamp() {
+  if (isDzhamping) return;
+  dino.classList.add("dzhamp");
+  isDzhamping = true;
+  setTimeout(() => {
+    dino.classList.remove("dzhamp");
+    isDzhamping = false;
+  }, 500);
 }
 
 document.addEventListener("click", dzhamp);
+const dino = document.querySelector(".dino");
+const kaktyc = document.querySelector(".kaktyc");
+let isDzhamping = false;
+
+function dzhamp() {
+  if (isDzhamping) return;
+  dino.classList.add("dzhamp");
+  isDzhamping = true;
+  setTimeout(() => {
+    dino.classList.remove("dzhamp");
+    isDzhamping = false;
+  }, 500);
+}
+
+document.addEventListener("click", dzhamp);
+const isDed = setInterval(() => {
+  const dinoKvadratik = dino.getBoundingClientRect();
+  const kaktycKvadratik = kaktyc.getBoundingClientRect();
+
+  if (
+    dinoKvadratik.right > kaktycKvadratik.left &&
+    dinoKvadratik.left < kaktycKvadratik.right &&
+    dinoKvadratik.bottom > kaktycKvadratik.top &&
+    dinoKvadratik.top < kaktycKvadratik.bottom
+  ) {
+    kaktyc.style.animation = "none";
+  } else {
+  }
+});
