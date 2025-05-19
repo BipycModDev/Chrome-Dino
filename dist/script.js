d33811f9ca29d3c91925815d50ed47d6dd97cb61
BipycModDev <ukrainianguy20@gmail.com>
2025-05-19 22:15:24 +0300

    05.19.2025

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,12 @@
 const dino = document.querySelector(".dino");
 const kaktyc = document.querySelector(".kaktyc");
+const scoreText = document.querySelector(".score-text");
+const gameOver = document.querySelector(".game-over");
+const finalScore = document.querySelector(".final-score");
+const button = document.querySelector("button");
 let isDzhamping = false;
+let score = 0;
+let isDedInterval;
 
 function dzhamp() {
   if (isDzhamping) return;
@@ -13,10 +19,6 @@ function dzhamp() {
 }
 
 document.addEventListener("click", dzhamp);
-const dino = document.querySelector(".dino");
-const kaktyc = document.querySelector(".kaktyc");
-let isDzhamping = false;
-
 function dzhamp() {
   if (isDzhamping) return;
   dino.classList.add("dzhamp");
@@ -28,17 +30,33 @@ function dzhamp() {
 }
 
 document.addEventListener("click", dzhamp);
-const isDed = setInterval(() => {
-  const dinoKvadratik = dino.getBoundingClientRect();
-  const kaktycKvadratik = kaktyc.getBoundingClientRect();
+function isDed() {
+  isDedInterval = setInterval(() => {
+    const dinoKvadratik = dino.getBoundingClientRect();
+    const kaktycKvadratik = kaktyc.getBoundingClientRect();
 
-  if (
-    dinoKvadratik.right > kaktycKvadratik.left &&
-    dinoKvadratik.left < kaktycKvadratik.right &&
-    dinoKvadratik.bottom > kaktycKvadratik.top &&
-    dinoKvadratik.top < kaktycKvadratik.bottom
-  ) {
-    kaktyc.style.animation = "none";
-  } else {
-  }
+    if (
+      dinoKvadratik.right > kaktycKvadratik.left &&
+      dinoKvadratik.left < kaktycKvadratik.right &&
+      dinoKvadratik.bottom > kaktycKvadratik.top &&
+      dinoKvadratik.top < kaktycKvadratik.bottom
+    ) {
+      clearInterval(isDedInterval);
+      kaktyc.style.animation = "none";
+      finalScore.textContent = "Final score: ${Math.floor(score / 100)}";
+      gameOver.style.display = "block";
+    } else {
+      score++;
+      scoreText.textContent = Math.floor(score / 100);
+    }
+  }, 15);
+}
+button.addEventListener("click", () => {
+  gameOver.style.display = "none";
+  score = 0;
+  scoreText.textContent = "0";
+  isDzhamping = false;
+  kaktyc.style.animation = "kaktyc 1.5s linear infinite";
+  isDed();
 });
+isDed();
